d468b1fdd2bb54c8d2853d049e6e09e427264849
do4fun <do4fun@hotmail.com>
2017-04-06 15:51:21 -0400

    create db directory and there database files; create first session server

--- session.php
+++ session.php
@@ -1,3 +1,13 @@
 <?php
 
+    $connection = mysqli_connect('127.0.0.1', 'root', '', 'github') or die("Error " . mysqli_error($connection));
+    $sql = "SELECT * FROM git_users";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result)){
+        $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+    mysqli_close($connection);
+
 ?>
