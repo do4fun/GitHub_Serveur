12b0435f2990684d7f556e3d3d30a2ba1467d0a3
do4fun <do4fun@hotmail.com>
2017-04-06 15:20:13 -0400

    fix SQL bugs

--- CreateDatabaseGitHub.sql
+++ CreateDatabaseGitHub.sql
@@ -2,6 +2,6 @@ drop table users;
 drop table logbook;
 drop table dispatch;
 
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTO_INCREMENT, username VARCHAR(), password VARCHAR(20), type VARCHAR(2));
-CREATE TABLE IF NOT EXISTS logbook (id INTEGER PRIMARY KEY AUTO_INCREMENT, userid INTEGER, date VARCHAR(20), arrivalHour VARCHAR(2), arrivalMinute VARCHAR(2), departureHour VARCHAR(2), departureMinute VARCHAR(2), activity VARCHAR(15));
-CREATE TABLE IF NOT EXISTS dispatch (id INTEGER PRIMARY KEY AUTO_INCREMENT, dispatchid INTEGER, userid VARCHAR(20) );
+CREATE TABLE IF NOT EXISTS git_users (id INTEGER PRIMARY KEY AUTO_INCREMENT, username VARCHAR(20), userpassword VARCHAR(20), usertype VARCHAR(2));
+CREATE TABLE IF NOT EXISTS git_logbook (id INTEGER PRIMARY KEY AUTO_INCREMENT, userid INTEGER, logbookdate VARCHAR(20), arrivalHour VARCHAR(2), arrivalMinute VARCHAR(2), departureHour VARCHAR(2), departureMinute VARCHAR(2), activity VARCHAR(15));
+CREATE TABLE IF NOT EXISTS git_dispatch (id INTEGER PRIMARY KEY AUTO_INCREMENT, dispatchid INTEGER, userid VARCHAR(20) );
