27056aa98e8fac83f0e4b827e3032774aa52852a
do4fun <do4fun@hotmail.com>
2017-04-06 15:23:23 -0400

    Add static duty status grid

---
+++ js/fichejournaliere.js
@@ -0,0 +1,15 @@
+
+$(document).ready(
+	function(){
+/*		$('#offduty #00h #padding #00m, #offduty #00h #padding #15m, #offduty #00h #padding #30m, #offduty #00h #padding #45m').addClass('template_size left_blank_template')
+
+		$('#offduty #00h #middle #00m, #offduty #00h #middle #15m, #offduty #00h #middle #30m, #offduty #00h #middle #45m').addClass('template_size left_blank_template')
+
+		$('#offduty #00h #bottom #45m, #offduty #1h #bottom #00m, #offduty #1h #bottom #15m, #offduty #1h #bottom #30m, #offduty #1h #bottom #45m').addClass('top_bold_template')
+		$('#offduty #2h #bottom #00m' ).addClass('left_bold_template')
+//		$('#offduty #2h #bottom #00m' ).css("width","-=2px")
+		$('#sleeper #2h #padding #00m').addClass('left_bold_template')
+//		$('#offduty #2h' ).css("height","-=2px")
+
+		$('#offduty').css("width","100%")
+	});
