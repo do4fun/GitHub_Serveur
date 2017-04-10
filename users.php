ba24faa266f65146d222e5406fd3b7e74dd11b5b
do4fun <do4fun@hotmail.com>
2017-04-09 20:26:48 -0400

    code optimisation

--- users.php
+++ users.php
@@ -3,47 +3,27 @@
 require_once( './db/connexion.php' );
 require_once( 'session.php' );
 	$connection = mysqli_connect(HOST, USER, PASS, DB) or die("Error " . mysqli_error($connection));
-
+	
 	if( !($_GET['sessionid'] == null) && sessionExist( $_GET['sessionid'] ) ){
 		if( !($_GET['isuser'] == null )){
 			$sql = "SELECT * FROM git_users WHERE id = " . $_GET['isuser'];
-		    $result = mysqli_query($connection, $sql);
-		    $array = array();
-		    while($row=mysqli_fetch_assoc($result)){
-		        $array[] = $row['id'];
-		    }
-		    echo json_encode($array);
 		} else if( !($_GET['isdispatch'] == null )){
 			$sql = "SELECT * FROM git_users WHERE usertype = '1' AND id = " . $_GET['isdispatch'];
-	//		echo $sql . "<br>";
-		    $result = mysqli_query($connection, $sql);
-		    $array = array();
-		    while($row=mysqli_fetch_assoc($result)){
-		        $array[] = $row['id'];
-		    }
-		    echo json_encode($array);
 		}else if( !($_GET['getdriveridof'] == null) ) {
 			$sql = "SELECT d.userid as id FROM git_users u, git_dispatch d WHERE d.dispatchid = u.id AND u.usertype = 1 AND u.id = " . $_GET['getdriveridof'];
-	//		echo $sql . "<br>";
-		    $result = mysqli_query($connection, $sql);
-		    $array = array();
-		    while($row=mysqli_fetch_assoc($result)){
-		        $array[] = $row['id'];
-		    }
-		    echo json_encode($array);
 		}else{
 			$sql = "SELECT * FROM git_users WHERE id = " . $_GET['id'];
-		    $result = mysqli_query($connection, $sql);
-		    $emparray = array();
-		    while($row=mysqli_fetch_assoc($result)){
-		        $emparray[] = $row;
-		    }
-		    echo json_encode($emparray);
 	    }
+	    $result = mysqli_query($connection, $sql);
+	    $array = array();
+	    while($row=mysqli_fetch_assoc($result)){
+	    	$array[] = $row['id'];
+	    }
+	    echo json_encode($array);
 	}else{
 		echo 'connection fail';
 	}
-	
+
     mysqli_close($connection);
 
 ?>
