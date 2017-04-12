3436961de324843f6bf681224597b5a2d1e3b3af
do4fun <do4fun@hotmail.com>
2017-04-11 20:53:06 -0400

    create, update user and use new function from session.php

--- users.php
+++ users.php
@@ -1,39 +1,50 @@
 <?php
+session_start();
 // This script is used to verify if user session exist then extract which user type he is
 	require_once( './db/connexion.php' );
 	require_once( 'session.php' );
-	$connection = mysqli_connect(HOST, USER, PASS, DB) or die("Error " . mysqli_error($connection));
-	
-	if( !($_GET['sessionid'] == null) && sessionExist( $_GET['sessionid'] ) ){
-		// Verify if the isuser passed in parameter in URL is a valid user id and is active
-		if( !($_GET['isuser'] == null )){
-			$sql = "SELECT * FROM git_users WHERE active = 1 and id = " . $_GET['isuser'];
-		// Verify if the isdispatch passed in parameter in URL is a valid dispatch id and is active
-		} else if( !($_GET['isdispatch'] == null )){
-			$sql = "SELECT * FROM git_users WHERE usertype = '1' AND id = " . $_GET['isdispatch'];
-		// Return all active drivers linked for a dispatch id passed in parameter in URL
-		}else if( !($_GET['getdriveridof'] == null) ) {
-			$sql = "SELECT d.userid as id, u.usertype as usertype FROM git_users u, git_dispatch d WHERE u.active = 1 AND d.active = 1 AND d.dispatchid = u.id AND u.usertype = 1 AND u.id = " . $_GET['getdriveridof'];
-	    }
-		// Execute SQL Query
-	    $result = mysqli_query($connection, $sql);
-	    $array = array();
-//	    echo $sql . "<br>";
-	    while($row=mysqli_fetch_assoc($result)){
-			// if the query return a result(s) a JSON result is build from data(s) and session variable is set with user type.
-	    	$array[] = $row['id'];
-	    	if($row['usertype'] == '1' ){
-	    		$_SESSION['usertype'] = "dispatch";
-	    	}else{
-	    		$_SESSION['usertype'] = "driver";
+//	$connection = mysqli_connect(HOST, USER, PASS, DB) or die("Error " . mysqli_error($connection));
+	if(( $_GET['sessionid'] != null) && sessionExist( $_GET['sessionid'] ) ){
+		// Verify type of user passed in parameter in URL is a valid user id and is active
+		if( $_GET['usertype'] != null ){
+			if( $_GET['userid'] != null ){
+				$sql = "SELECT * FROM git_users WHERE active = 1 and id = " . $_GET['userid'];
+			} 
+		    
+		    $result = getSQLResult($sql);
+		    if(rowCounterOf( $result ) >= 1){
+		    	if(getSingleRecordForColumn( $result, 'usertype' ) == '1' ){
+		    		$_SESSION['usertype'] = "dispatch";
+		    	}else{
+		    		$_SESSION['usertype'] = "driver";
+		    	}
+		    	echo getJSONFormat( $result ) . "<br>";
+		    }
+		// Update driver's data from user id, username and password passed in URL parameters
+		}else if( $_GET['updatedriver'] != null && $_GET['userid'] != null){
+	    	if( $_GET['username'] != null ){
+	    		$sql = "UPDATE git_users SET username = " . $_GET['username'] . " WHERE id = " . $_GET['userid'] ;
+	    	} else if( $_GET['password'] != null ){
+	    		$sql = "UPDATE git_users SET password = " . $_GET['username'] . " WHERE id = " . $_GET['userid'] ;
+	    	} 
+	    	if (executeSQL($sql) == true) {
+	    		echo "OK";
+	    	} else {
+	    		echo "Error: " . $sql . "<br>" . $conn->error;
 	    	}
+	    }else{
+	    	echo 'userid is required';
 	    }
-//	    echo $_SESSION['usertype'] . "<br>";
-	    echo json_encode($array);
-	}else{
-		echo 'connection fail';
+	}else {
+		echo 'Unavailable session id for this user ';
 	}
 
-    mysqli_close($connection);
-
+	
+	/*
+	 echo getJSONFormat( $result ) . '<br>';
+	 echo getJSONFormatForColumn( $result, 'usertype' ) . '<br>';
+	 echo getSingleRecordForColumn( $result, 'usertype' ) . '<br>';
+	 echo rowCounterOf( $result );
+	 */
+	
 ?>
